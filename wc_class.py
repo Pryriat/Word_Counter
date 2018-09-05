5311639bedf48278fb73016b72b15c73a03f9ffd
Chernobyl <450342251@qq.com>
2018-09-05 12:24:45 +0800

    基础框架搭建

---
+++ wc_class.py
@@ -0,0 +1,46 @@
+class word_counter:
+	def isnull(file):
+	'''
+	判断文件是否为空
+	'''
+		t = 0
+		for lines in  file.readlines():
+			t+=1
+		if t == 0:
+			return false
+		return true
+		
+	def char_count(string_line):
+		char_num = 0
+		for x in string_line:
+			
+	'''
+	文件字符数统计
+	'''
+	def word_count(string_line):
+	'''
+	文件单词数统计
+	'''
+	def special_count(string_line):
+	'''
+	特殊字符统计
+	'''
+	def comment_count(string——line）
+	def file_process():
+	'''
+	文件处理主函数
+	'''
+	def file_select():
+	'''
+	文件的选择、通配符处理
+	'''
+	def ui_main():
+	'''
+	图形化界面的显示
+	'''
+	def command_line_proc()
+	'''
+	命令行参数处理
+	'''
+	
+		
