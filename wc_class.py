fcf583f3b8214e125470bd1d014ff0619d7343f3
Chernobyl <450342251@qq.com>
2018-09-15 00:35:53 +0800

    修复问题

--- wc_class.py
+++ wc_class.py
@@ -18,6 +18,17 @@ class word_counter:
 		self.file = ''
 		self.opt=opt
 
+	def clear(self):
+		self.char_num = 0
+		self.word_num = 0
+		self.lines = 0
+		self.nullline = 0
+		self.single_comment = 0
+		self.multi_comment = 0
+		self.code_line = 0
+		self.is_null = False
+		self.file = ''
+
 	def isnull(self,string):
 		'''
 		判断文件是否为空
@@ -44,15 +55,18 @@ class word_counter:
 
 	def line_count(self,string):
 		'''
-		文件行数统计与空行统计
+		文件行数统计、空行统计与代码行统计
 		'''
 		null_line = 0
-		sepcial_pattern = re.compile('.*\n')
+		code_line = 0
+		sepcial_pattern = re.compile('.*\n?')
 		tmp_result = sepcial_pattern.findall(string)
 		for lines in tmp_result:
-			if len(lines.strip()) <= 1:
+			if len(lines.strip()) == 0 or (len(lines.strip()) == 1 and  lines.strip() in ['{','}','#','/','\\','(',')']):
 				null_line += 1
-		return (len(tmp_result),null_line)
+			elif len(lines.strip())>=1 and lines[0]!= '/':
+				code_line += 1
+		return (len(tmp_result)-1,null_line-1,code_line)
 
 	def single_comment_count(self,string):
 		'''
@@ -66,7 +80,6 @@ class word_counter:
 		for lines in tmp_result1:
 			if len(multi_in_single_pattern.findall(lines)) == 1:
 				self.multi_comment -= 1
-		print('sig',len(tmp_result1) - len(tmp_result2))
 		return len(tmp_result1) - len(tmp_result2)
 
 
@@ -76,10 +89,13 @@ class word_counter:
 		'''
 		multi_lines = 0
 		del_lines = 0
+		flag = 0
 		multi_pattern = re.compile('/\*+[^\*]+\*/')
 		single_in_multi_pattern = re.compile('//.*')
 		del_pattern = re.compile('"[^"]*/\*[^"]*\*/')
 		tmp_result = multi_pattern.findall(string)
+		if len(tmp_result)>0:
+			flag = 1
 		for result1 in tmp_result:
 			self.single_comment -= len(single_in_multi_pattern.findall(result1))
 			for x in result1:
@@ -89,7 +105,7 @@ class word_counter:
 		for x in del_result:
 			if x == '\n':
 				del_lines += 1
-		return multi_lines - del_lines
+		return multi_lines+flag - del_lines
 
 	def file_process(self,file):
 		'''
@@ -106,8 +122,8 @@ class word_counter:
 			self.single_comment += self.single_comment_count(self.file_string)
 			self.char_num += self.char_count(self.file_string)
 			self.word_num += self.word_count(self.file_string)
-			(self.lines,self.nullline) = self.line_count(self.file_string)
-			self.code_line = self.lines-self.single_comment-self.multi_comment-self.nullline
+			(self.lines,self.nullline,self.code_line) = self.line_count(self.file_string)
+			self.code_line-=-self.multi_comment
 			if self.opt['a'] :
 				print('character:'+str(self.char_num),end = ' ')
 				print('words:'+str(self.word_num),end=' ')
@@ -131,3 +147,4 @@ class word_counter:
 		file_list = glob.glob(self.opt['f'])
 		for x in file_list:
 			self.file_process(x)
+			self.clear()
